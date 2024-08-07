72ed3d06c69c9b6adae262b96d9b83f931356abb
Aaron Kaw <aaronjkaw@gmail.com>
2024-08-07 13:54:52 +1000

    I missed one

--- test/99_postliminary/abstract_trees.jl
+++ test/99_postliminary/abstract_trees.jl
@@ -3,5 +3,6 @@ using Test
 
 buf = IOBuffer()
 @test print_tree(buf, SonarType) |> isnothing
-@test print_tree(buf, OceanSonar.AbstractModeller) |> isnothing
+@test print_tree(buf, OceanSonar.ModellingType) |> isnothing
+@test print_tree(buf, OceanSonar.EnvironmentComponent) |> isnothing
 @test !isempty(buf |> take! |> String)
