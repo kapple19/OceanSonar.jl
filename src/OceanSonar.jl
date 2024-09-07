de4cba8d46a6c2f1a92aa300dd7911f3408df6ab
Aaron Kaw <aaronjkaw@gmail.com>
2024-09-07 14:24:58 +1000

    Automated lexicographical filesystem hierarchy inclusion

--- src/OceanSonar.jl
+++ src/OceanSonar.jl
@@ -7,6 +7,37 @@ Package of ocean sonar numerical models.
 """
 module OceanSonar
 
-# Write your package code here.
+"""
+`include_subroots`
+
+Automates lexicographical `include`-sion of filesystem hierarchy.
+
+TODO: Generalise.
+"""
+function include_subroots(current_path::AbstractString)
+    current_directory = if isfile(current_path)
+        dirname(current_path)
+    elseif isdir(current_path)
+        current_path
+    else
+        error("What is ", current_path, "?")
+    end
+
+    for subpath in readdir(current_directory, join = true, sort = true)
+        if isdir(subpath)
+            include_subroots(subpath)
+            continue
+        elseif isfile(subpath)
+            endswith(subpath, "_.jl") && continue
+            subpath == current_path && continue
+            # @info "Including: $subpath"
+            subpath
+        else
+            error("What is ", subpath, "?")
+        end |> include
+    end
+end
+
+include_subroots(@__FILE__)
 
 end
